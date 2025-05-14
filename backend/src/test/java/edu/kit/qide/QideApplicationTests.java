847056af977eff77395c042a290dc5f56d90aeca
Henrik K <contact@Henrik-K.de>
2025-05-14 11:35:16 +0200

    Initialize project
    Both frontend and backend have their standard project setup.

---
+++ backend/src/test/java/edu/kit/qide/QideApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.kit.qide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
