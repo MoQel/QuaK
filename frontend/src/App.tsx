cdcfac9b8b101345b3acb523166dce4960b2a793
JaferShar <jafer.shar@gmail.com>
2025-06-18 15:14:10 +0200

    Created an abstract outline and added text editor

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
-import './App.css'
 import {useEffect} from "react";
-import ModelView from "@/components/ModelView.tsx";
-import {Button} from "@/components/ui/button.tsx";
+import styles from './App.module.css'
+import {ResizableHandle, ResizablePanel, ResizablePanelGroup} from "@/components/ui/resizable.tsx";
+import QLPEditor from "./components/ui/QLPEditor";
 
 function App() {
     useEffect(() => {
@@ -9,11 +9,38 @@ function App() {
     }, []);
     return (
         <>
-            <div className="flex flex-col h-screen">
-                <div className="flex-1 overflow-auto">
-                    <ModelView/>
+            <div className="flex flex-col h-screen px-[10px]">
+                <div className="flex flex-row h-2/3">
+                    <div className="border-2 border-amber-200 w-1/4">project manager</div>
+                    <div className="flex flex-grow-[2] w-full">
+                        {/*<div className="border-2 border-amber-200 w-3/4">circuit-view*/}
+                        {/*    <span className={styles.lines}>*/}
+                        {/*            <span className={styles.gate}>*/}
+                        {/*                X*/}
+                        {/*            </span>*/}
+                        {/*    </span>*/}
+                        {/*</div>*/}
+                        {/*<div className="border-2 border-amber-200 w-1/4">text-editor</div>*/}
+                        <ResizablePanelGroup direction="horizontal">
+                            <ResizablePanel>
+                                <span className={styles.lines}>
+                                    <span className={styles.gate}>
+                                        X
+                                    </span>
+                                </span>
+                            </ResizablePanel>
+                            <ResizableHandle withHandle/>
+                            <ResizablePanel>
+                                <QLPEditor/>
+                            </ResizablePanel>
+                        </ResizablePanelGroup>
+                    </div>
                 </div>
-                <Button variant="secondary" className="cursor-pointer">Transform</Button>
+                <div className="flex flex-grow-[1] flex-row w-full">
+                    <div className="border-2 border-amber-200 w-full">library</div>
+                    <div className="border-2 border-amber-200 w-full">inspector</div>
+                    <div className="border-2 border-amber-200 w-full">results</div>
+                </div>
             </div>
         </>
     )
