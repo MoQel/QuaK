08ebea259a0cd02bb13a12543dd74f47dc620001
JaferShar <jafer.shar@gmail.com>
2025-06-18 15:21:16 +0200

    Added a gate Library with only dummy Gate

---
+++ frontend/src/components/GateLibrary.tsx
@@ -0,0 +1,12 @@
+import styles from '../App.module.css'
+
+export function GateInfoElement(){
+
+    return (
+        <p draggable="true"
+           className={styles.availableGate}>
+            X
+        </p>
+    )
+
+}
