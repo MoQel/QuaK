7f891e2b7c7c38d79bc36f27a0586204bc6c85de
JaferShar <jafer.shar@gmail.com>
2025-06-19 13:09:03 +0200

    Updated plain css views to shadcn cards

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import styles from './App.module.css'
 import {ResizableHandle, ResizablePanel, ResizablePanelGroup} from "@/components/ui/resizable.tsx";
 import QLPEditor from "./components/ui/QLPEditor";
 import GateLibrary from "./components/GateLibrary";
+import {Card, CardContent} from "@/components/ui/card.tsx";
 
 function App() {
     useEffect(() => {
@@ -16,27 +17,45 @@ function App() {
                     <div className="flex flex-grow-[2] w-full">
                         <ResizablePanelGroup direction="horizontal">
                             <ResizablePanel>
+                                <Card className="h-full">
+                                    <CardContent>
                                 <span className={styles.lines}>
                                     <span className={styles.gate}>
                                         X
                                     </span>
                                 </span>
+                                    </CardContent>
+                                </Card>
                             </ResizablePanel>
                             <ResizableHandle withHandle/>
                             <ResizablePanel>
-                                <QLPEditor/>
+                                <Card className="h-full">
+                                    <CardContent>
+                                        <QLPEditor/>
+                                    </CardContent>
+                                </Card>
                             </ResizablePanel>
                         </ResizablePanelGroup>
                     </div>
                 </div>
                 <div className="flex flex-grow-[1] flex-row w-full">
-                    <div className="border-2 border-amber-200 w-full">
-                        <div className={styles.availableGateContainer}>
-                            <GateLibrary/>
-                        </div>
-                    </div>
-                    <div className="border-2 border-amber-200 w-full">inspector</div>
-                    <div className="border-2 border-amber-200 w-full">results</div>
+                    <Card className="w-full">
+                        <CardContent>
+                            <div className={styles.availableGateContainer}>
+                                <GateLibrary/>
+                            </div>
+                        </CardContent>
+                    </Card>
+                    <Card className="w-full">
+                        <CardContent>
+                            inspector
+                        </CardContent>
+                    </Card>
+                    <Card className="w-full">
+                        <CardContent>
+                            results
+                        </CardContent>
+                    </Card>
                 </div>
             </div>
         </>
