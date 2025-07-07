a50cf38e9603a53baf151e3c6ca8040fba5d1f16
JaferShar <106037863+JaferShar@users.noreply.github.com>
2025-07-07 15:06:40 +0300

    Added the wires to the UI

--- frontend/src/views/circuit-view/CircuitView.tsx
+++ frontend/src/views/circuit-view/CircuitView.tsx
@@ -1,10 +1,24 @@
 import {Card, CardContent} from "@/components/ui/card.tsx";
 import styles from "@/App.module.css";
+import {QuantumWires} from "@/views/circuit-view/QuantumWires.tsx";
 
 export function CircuitView() {
+    let length = 1000
+    let qubits = 3
     return (
-        <Card className="h-full">
+        <Card className="h-full overflow-scroll">
             <CardContent>
+                <div className="">
+                    <div>
+                        {Array.from({length: qubits}).map((_, qubitIndex) => (
+                            <QuantumWires
+                                qubitIndex={qubitIndex}
+                                length={length}
+                            />
+                        ))
+                        }
+                    </div>
+                </div>
             </CardContent>
         </Card>
     )
