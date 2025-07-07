27f29456f0143786061b496afe5b6df0f015c00a
JaferShar <106037863+JaferShar@users.noreply.github.com>
2025-07-07 16:39:32 +0300

    User can now add/remove qubits from ui

--- frontend/src/views/circuit-view/CircuitView.tsx
+++ frontend/src/views/circuit-view/CircuitView.tsx
@@ -2,19 +2,43 @@ import {Card, CardContent} from "@/components/ui/card.tsx";
 import {QuantumWires} from "@/views/circuit-view/QuantumWires.tsx";
 import {Button} from "@/components/ui/button.tsx";
 import {Minus, Plus, Trash} from "lucide-react";
+import {CircuitState} from "@/type/quantum.tsx";
+import {useCallback, useState} from "react";
 
 export function CircuitView() {
-    let length = 1000
-    let qubits = 3
+    const WIRE_LENGTH = 1000
+
+    const [circuitState, setCircuitState] = useState<CircuitState>({
+        qubits: 3,
+        steps: 20,
+    });
+
+    const removeQubit = useCallback(() => {
+        console.log("Minus pressed")
+        setCircuitState(prev => ({
+            qubits: Math.max(prev.qubits - 1, 1),
+            steps: prev.steps,
+        }));
+    }, []);
+
+    const addQubit = useCallback(() => {
+        console.log("Minus pressed")
+        setCircuitState(prev => ({
+            qubits: Math.min(prev.qubits + 1, 20),
+            steps: prev.steps,
+        }));
+    }, []);
+
+
     return (
         <Card className="h-full overflow-scroll">
             <CardContent>
                 <div className="">
                     <div className="pb-5 flex justify-end space-x-3">
-                        <Button size="icon" className="size-8">
+                        <Button onClick={addQubit} size="icon" className="size-8">
                             <Plus/>
                         </Button>
-                        <Button size="icon" className="size-8">
+                        <Button onClick={removeQubit} size="icon" className="size-8">
                             <Minus/>
                         </Button>
                         <Button size="icon" className="size-8">
@@ -22,10 +46,10 @@ export function CircuitView() {
                         </Button>
                     </div>
                     <div>
-                        {Array.from({length: qubits}).map((_, qubitIndex) => (
+                        {Array.from({length: circuitState.qubits}).map((_, qubitIndex) => (
                             <QuantumWires
                                 qubitIndex={qubitIndex}
-                                length={length}
+                                length={WIRE_LENGTH}
                             />
                         ))
                         }
