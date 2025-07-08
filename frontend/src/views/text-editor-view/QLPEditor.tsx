964047978d151f1c37bf00d028e7744e6136b740
JaferShar <106037863+JaferShar@users.noreply.github.com>
2025-07-08 15:58:42 +0300

    Installed drag&drop library

--- frontend/src/views/text-editor-view/QLPEditor.tsx
+++ frontend/src/views/text-editor-view/QLPEditor.tsx
@@ -7,29 +7,31 @@ function QLPEditor() {
     const [value, setValue] = useState('');
     const [language, setLanguage] = useState("python");
 
-    const onSelect = (language : string) => {
+    const onSelect = (language: string) => {
         setLanguage(language);
         toast("Language " + language);
     }
-    return <div className="h-full flex flex-col">
-        <div className="shrink-0">
-            <LanguageSelector language={language} onSelect={onSelect}/>
+    return (
+        <div className="h-full flex flex-col">
+            <div className="shrink-0">
+                <LanguageSelector language={language} onSelect={onSelect}/>
+            </div>
+            <div className="h-full">
+                <Editor
+                    language={language}
+                    defaultValue="# Some Quantum Code"
+                    theme="vs-dark"
+                    value={value}
+                    onChange={(value) => setValue(value || '')}
+                    options={{
+                        minimap: {enabled: false},
+                        wordWrap: 'on',
+                    }}
+                    height="100%"
+                />
+            </div>
         </div>
-        <div className="h-full">
-            <Editor
-                language={language}
-                defaultValue="# Some Quantum Code"
-                theme="vs-dark"
-                value={value}
-                onChange={(value) => setValue(value || '')}
-                options={{
-                    minimap: {enabled: false},
-                    wordWrap: 'on',
-                }}
-                height="100%"
-            />
-        </div>
-    </div>;
+    );
 }
 
 export default QLPEditor;
