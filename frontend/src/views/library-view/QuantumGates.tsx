df99a4f0e44304061587c07764991f40e56087c8
JaferShar <jafer.shar@gmail.com>
2025-07-23 13:57:57 +0200

    Renaming grammar error

--- frontend/src/views/library-view/QuantumGates.tsx
+++ frontend/src/views/library-view/QuantumGates.tsx
@@ -5,41 +5,41 @@ export const quantumGates: QuantumGate[] = [
     {
         id: 'hadamard',
         type: 'H',
-        qubits: 1,
+        qubit: 1,
     },
     {
         id: 'pauli-x',
         type: 'X',
-        qubits: 1,
+        qubit: 1,
     },
     {
         id: 'pauli-y',
         type: 'Y',
-        qubits: 1,
+        qubit: 1,
     },
     {
         id: 'pauli-z',
         type: 'Z',
-        qubits: 1,
+        qubit: 1,
     },
     {
         id: 'cnot',
         type: 'CNOT',
-        qubits: 2,
+        qubit: 2,
     },
     {
         id: 's-gate',
         type: 'S',
-        qubits: 1,
+        qubit: 1,
     },
     {
         id: 't-gate',
         type: 'T',
-        qubits: 1,
+        qubit: 1,
     },
     {
         id: 'measure',
         type: 'MEASURE',
-        qubits: 1,
+        qubit: 1,
     }
 ]
