319ed7f3305dad30c854e868862dc9629f4ea6c0
JaferShar <jafer.shar@gmail.com>
2025-07-23 13:56:49 +0200

    An array that contains the initial state of the circuit

---
+++ frontend/src/views/circuit-view/InitCircuit.tsx
@@ -0,0 +1,55 @@
+import {QuantumGate} from "@/views/library-view/QuantumGate.tsx";
+
+export const quantumGates: QuantumGate[] = [
+
+    {
+        id: 'hadamard',
+        type: 'H',
+        qubit: 1,
+    },
+    {
+        id: 'pauli-x',
+        type: 'X',
+        qubit: 1,
+    },
+    {
+        id: 'pauli-y',
+        type: 'Y',
+        qubit: 1,
+    },
+    {
+        id: 'pauli-z',
+        type: 'Z',
+        qubit: 1,
+    },
+    {
+        id: 'pauli-y',
+        type: 'Y',
+        qubit: 2,
+    },
+    {
+        id: 'pauli-z',
+        type: 'Z',
+        qubit: 2,
+    },
+    {
+        id: 'pauli-y',
+        type: 'Y',
+        qubit: 3,
+    },
+    {
+        id: 'pauli-z',
+        type: 'Z',
+        qubit: 3,
+    },
+    {
+        id: 'hadamard',
+        type: 'H',
+        qubit: 3,
+    },
+    {
+        id: 'pauli-x',
+        type: 'X',
+        qubit: 3,
+    },
+]
