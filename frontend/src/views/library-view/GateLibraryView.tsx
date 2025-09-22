0d830644181a8b30e60af6cad22ce548d79fdcbf
JaferShar <jafer.shar@gmail.com>
2025-09-22 20:10:08 +0200

    Added  a title to library view

--- frontend/src/views/library-view/GateLibraryView.tsx
+++ frontend/src/views/library-view/GateLibraryView.tsx
@@ -1,10 +1,15 @@
-import {Card, CardContent} from "@/components/ui/card.tsx";
+import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card.tsx";
 import styles from "@/App.module.css";
 import GateLibrary from "@/views/library-view/GateLibrary.tsx";
 
 export function GateLibraryView() {
     return (
         <Card className="w-full">
+            <CardHeader className="w-full">
+                <CardTitle>
+                    Library
+                </CardTitle>
+            </CardHeader>
             <CardContent>
                 <div className={styles.availableGateContainer}>
                     <GateLibrary/>
