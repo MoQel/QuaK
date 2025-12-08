ee6c1db815710dd40f31953d328a721d8f0600ad
Nick Oelmann <nick.oelmann@web.de>
2025-12-08 16:25:59 +0100

    #57: Consistent validation of DTOs

--- backend/src/main/java/edu/kit/quak/infrastructure/filesystem/in/web/rest/dto/FileContentRequest.java
+++ backend/src/main/java/edu/kit/quak/infrastructure/filesystem/in/web/rest/dto/FileContentRequest.java
@@ -1,6 +1,12 @@
 package edu.kit.quak.infrastructure.filesystem.in.web.rest.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record FileContentRequest(
+        @NotNull
         byte[] content,
+
+        @NotBlank
         String contentType
 ) {}
--- backend/src/main/java/edu/kit/quak/infrastructure/filesystem/in/web/rest/dto/RenameFileRequest.java
+++ backend/src/main/java/edu/kit/quak/infrastructure/filesystem/in/web/rest/dto/RenameFileRequest.java
@@ -1,8 +1,8 @@
 package edu.kit.quak.infrastructure.filesystem.in.web.rest.dto;
 
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotBlank;
 
 public record RenameFileRequest(
-        @Size(min = 1, message = "Name must not be empty.")
+        @NotBlank
         String name
 ) {}
