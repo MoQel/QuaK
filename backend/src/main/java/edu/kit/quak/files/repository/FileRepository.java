d682e1ff4a4d7ae3f8bde141cf95475294479aef
simon.vonroenn <simon.vonroenn@web.de>
2025-12-17 16:44:58 +0100

    #62: Delete old unused code

--- backend/src/main/java/edu/kit/quak/files/repository/FileRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.kit.quak.files.repository;
-
-import edu.kit.quak.files.model.File;
-import org.springframework.data.repository.CrudRepository;
-
-public interface FileRepository extends CrudRepository<File, String> {
-}
--- backend/src/main/java/edu/kit/quak/files/repository/ProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.kit.quak.files.repository;
-
-import edu.kit.quak.files.model.Project;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProjectRepository extends CrudRepository<Project, String> {
-}
