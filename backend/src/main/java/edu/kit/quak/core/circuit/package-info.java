9e7ca33d9ba15d1eccfc276af7dc3fddffab9f8f
simon.vonroenn <simon.vonroenn@web.de>
2026-01-03 14:55:10 +0100

    #62: Add link to paper to package-info

--- backend/src/main/java/edu/kit/quak/core/circuit/package-info.java
+++ backend/src/main/java/edu/kit/quak/core/circuit/package-info.java
@@ -2,7 +2,7 @@
  * This package implements the quantum circuit metamodel proposed by Gemeinhardt et al. (2024),
  * "A Model-Driven Framework for Composition-Based Quantum Circuit Design".
  * <p>
- * The architecture is only based on the metamodel and does not fully adopt every aspect of it.
+ * The architecture is based on the metamodel but does not adopt every aspect of it.
  * Aspects, that have been adopted, include:
  * <ul>
  *   <li>Quantum circuits</li>
@@ -13,5 +13,7 @@
  * Reference:
  * F. Gemeinhardt, A. Garmendia, M. Wimmer, R. Wille, "A Model-Driven Framework for Composition-Based Quantum Circuit Design",
  * Johannes Kepler University Linz, 2024.
+ *
+ * @see <a href="https://dl.acm.org/doi/10.1145/3688856">ACM Digital Library: A Model-Driven Framework for Composition-Based Quantum Circuit Design</a>
  */
 package edu.kit.quak.core.circuit;
