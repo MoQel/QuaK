89441e1f2553899a3d30c57e51908fd461a2fe37
Furkan Cevik <furkan.cevik@mercedes-benz.com>
2026-01-22 23:11:06 +0100

    #57: Add GitHub Action for automatic Java formatting and update README documentation

--- backend/src/main/java/edu/kit/quak/QuaKApplication.java
+++ backend/src/main/java/edu/kit/quak/QuaKApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QuaKApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(QuaKApplication.class, args);
+        SpringApplication
+                .run(QuaKApplication.class, args);
     }
 }
