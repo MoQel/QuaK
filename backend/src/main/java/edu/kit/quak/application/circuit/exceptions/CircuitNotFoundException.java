a01ec95f7e370c120a119149a97af2d2498e4ff4
Nick Oelmann <nick.oelmann@web.de>
2026-03-03 11:46:20 +0100

    rerun spotless with cleared cache

--- backend/src/main/java/edu/kit/quak/application/circuit/exceptions/CircuitNotFoundException.java
+++ backend/src/main/java/edu/kit/quak/application/circuit/exceptions/CircuitNotFoundException.java
@@ -3,6 +3,7 @@ package edu.kit.quak.application.circuit.exceptions;
 import edu.kit.quak.application.common.exceptions.ResourceNotFoundException;
 
 public class CircuitNotFoundException extends ResourceNotFoundException {
+
     public CircuitNotFoundException(String id) {
         super("Circuit", id);
     }
