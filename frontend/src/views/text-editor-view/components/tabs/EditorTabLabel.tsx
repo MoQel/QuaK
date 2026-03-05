4a6f480a2b8c9ee433aa9d3e989acd3b27659f0b
Nick Oelmann <nick.oelmann@web.de>
2026-03-05 14:45:52 +0100

    #42: unify style

--- frontend/src/views/text-editor-view/components/tabs/EditorTabLabel.tsx
+++ frontend/src/views/text-editor-view/components/tabs/EditorTabLabel.tsx
@@ -21,8 +21,8 @@ export const EditorTabLabel = React.forwardRef<HTMLDivElement, EditorTabLabelPro
                 className={cn(
                     'group relative flex h-full rounded-none min-w-[120px] max-w-[200px] cursor-pointer select-none items-center px-3 text-sm font-medium transition-colors',
                     isActive
-                        ? 'bg-subtle text-text border-b-3'
-                        : 'bg-transparent text-text-muted hover:bg-bg hover:text-text border-b-3 border-b-transparent',
+                        ? 'bg-subtle text-text border-b-2'
+                        : 'bg-transparent text-text-muted hover:bg-bg hover:text-text border-b-2 border-b-transparent',
                     isActive && (isThisGroupFocused ? 'border-b-blue-500' : 'border-b-gray-500'),
                     className,
                 )}
